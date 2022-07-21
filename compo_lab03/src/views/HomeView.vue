a869afab1802caa12b8ff574f737eb35420f4ac9
Padungkiat <aomsinkaky14@gmail.com>
2022-07-21 23:32:36 +0700

    4.12

--- compo_lab03/src/views/HomeView.vue
+++ compo_lab03/src/views/HomeView.vue
@@ -2,16 +2,20 @@
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
+  <div class="category">
+    <Categories v-for="event in events" :key="event.id" :event="event" />
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
-
+import Categories from '@/components/Categories.vue'
 export default {
   name: 'HomeView',
   components: {
-    EventCard //register it as a child component
+    EventCard, //register it as a child component
+    Categories
   },
   data() {
     return {
@@ -20,34 +24,34 @@ export default {
           id: 5928101,
           category: 'animal welfare',
           title: 'Cat Adoption Day',
-          ' description': 'Find your new feline friend at this event.',
+          description: 'Find your new feline friend at this event.',
           location: 'Meow Town',
           date: 'January 28, 2022',
           time: '12:00',
           petsAllowed: true,
-          ' organizer': 'Kat Laydee'
+          organizer: 'Kat Laydee'
         },
         {
           id: 4582797,
           category: 'food',
           title: 'Community Gardening',
-          ' description': 'Join us as we tend to the community edible plants.',
+          description: 'Join us as we tend to the community edible plants.',
           location: 'Flora City',
           date: 'March 14, 2022',
           time: '10:00',
           petsAllowed: true,
-          ' organizer': 'Fern Pollin'
+          organizer: 'Fern Pollin'
         },
         {
           id: 8419988,
           category: 'sustainability',
           title: 'Beach Cleanup',
-          ' description': 'Help pick up trash along the shore.',
+          description: 'Help pick up trash along the shore.',
           location: 'Playa Del Carmen',
           date: 'July 22, 2022',
           time: '11:00',
           petsAllowed: false,
-          ' organizer': 'Carey Wales'
+          organizer: 'Carey Wales'
         }
       ]
     }
@@ -59,6 +63,12 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.category {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: right;
 }
 h4 {
   font-size: 20px;
